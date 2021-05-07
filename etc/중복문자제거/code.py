34e6dbac4fca61c69fbb9043bac87b0c2441dae0
이동근 <eagle5424228@github.com>
2021-05-07 18:43:37 +0900

    20210507 중복문자 제거

---
+++ etc/중복문자제거/code.py
@@ -0,0 +1,18 @@
+import collections
+
+def removeDuplicateLetters(s):
+    counter, seen, stack = collections.Counter(s), set(), []
+
+    for char in s:
+        counter[char] -= 1
+        
+        # 중복제거
+        if char in seen:
+            continue
+
+        while stack and char < stack[-1] and counter[stack[-1]] >0:
+            seen.remove(stack.pop())
+        stack.append(char)
+        seen.add(char)
+    return ''.join(stack)
+
