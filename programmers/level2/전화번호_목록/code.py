14c3363ac95c4ac62a99f37e9bdc558194d89d1b
gledong12 <gledong12@gmail.com>
2021-05-12 13:43:12 +0900

    code.py 성공한 코드 추가

--- programmers/level2/전화번호_목록/code.py
+++ programmers/level2/전화번호_목록/code.py
@@ -1,3 +1,4 @@
+# 반복문 사용하기
 def solution(phone_book):    
     for i in phone_book:
         num_data = i
@@ -12,3 +13,16 @@ def solution(phone_book):
 
 
 # 런타임 에러 발생, 몇 개의 다른 테스트 케이스에서 제대로 발생하지 않았다.
+
+# 정렬사용하기
+def solution(phone_book):
+    phone_book.sort()
+
+    for i, phone in enumerate(phone_book[:-1]):
+        if phone == phone[i+1][:len(phone)]:
+            return False
+    return True
+
+
+# 성공! 
+
